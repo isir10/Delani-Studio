8f5b384574226a852ffcccdf1ff4adb3bcd6ef29
isir10 <isir.malak@moringaschool.com>
2022-03-15 13:19:31 +0300

    added toggle effect to design icon

--- JS/scripts.js
+++ JS/scripts.js
@@ -33,3 +33,13 @@ $("form#form1").on('submit',function(event){
       $("#product-image").slideDown('1500');
     });
   });
+  $(document).ready(function(){
+    $("#design-image").click(function(){
+      $("#design-image").slideDown('1500').hide('1000');
+      $("#design").show('1500');
+    });
+    $("#design").click(function(){
+      $("#design").slideUp('1500');
+      $("#design-image").slideDown('1500');
+    });
+  });
