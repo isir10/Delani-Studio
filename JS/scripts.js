88ea69a9f53d6922420794488d72ec86a9608f85
isir10 <isir.malak@moringaschool.com>
2022-03-15 13:17:42 +0300

    added toggle effect to product management icon

--- JS/scripts.js
+++ JS/scripts.js
@@ -23,3 +23,13 @@ $("form#form1").on('submit',function(event){
       $("#development-image").slideDown('1500');
     });
   });
+  $(document).ready(function(){
+    $("#product-image").click(function(){
+      $("#product-image").slideDown('1500').hide('1000');
+      $("#product").show('1500');
+    });
+    $("#product").click(function(){
+      $("#product").slideUp('1500');
+      $("#product-image").slideDown('1500');
+    });
+  });
