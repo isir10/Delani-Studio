6a3bd34e82cd431f3480c22d2bd0a971bc12ae94
isir10 <isir.malak@moringaschool.com>
2022-03-15 11:56:03 +0300

    added alert function to js file

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,8 +1,16 @@
+$("form#form1").on('submit',function(event){
+    event.preventDefault();
+    var name = $("input#name").val();
+    var email = $("input#email").val();
+    var message = $("textarea#message").val();
 
-
-    toggleWhatWeDo();
+    if ($("input#name").val() && $("input#email").val() && $("input#message").val() ){
+        alert ("Hello " + name + ", Thank you for reaching out, we'll get back to you shortly...");
+    }
+    else if($("input#name").val() && $("input#email").val()){
+        alert("Thank you for reaching ou to us. If you wish to leave a message please type in on the message section..");
+    }
+   else{
+     alert("Please provide correct name or email")
+   }
 });
-
-// function
-
-//  function to toggle what we do
