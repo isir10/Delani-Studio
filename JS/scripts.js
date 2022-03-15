5e7a049d5e45bd47d90e5921ef8ced2548b18145
isir10 <isir.malak@moringaschool.com>
2022-03-15 13:13:32 +0300

    re-added html, js and css files

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,65 +1,25 @@
-var contactForm = $("#contact-form");
+$("form#form1").on('submit',function(event){
+    //event.preventDefault();
+    var name = $("input#name").val();
+    var email = $("input#email").val();
+    var message = $("textarea#message").val();
 
-$(function() {
-    // what we do titles add bold class and center
-    $('.w-title').addClass("fw-bold text-center");
+    if ($("input#name").val() && $("input#email").val()){
+        alert (name +", we have received your message. Thank you for reaching to us.");
+    }
+    else {
+        alert("please enter your name and email!");
+    }
 
-    contactForm.on("submit", function(e) {
-        e.preventDefault();
-        let formValid = validateContactForm();
-        if (formValid) {
-            const name = $("input.name").val();
+ });
 
-            alert(`Thank you ${name}, your message has been received`);
-            clearForm();
-        }
+ $(document).ready(function(){
+    $("#development-image").click(function(){
+      $("#development-image").slideDown('1500').hide('1000');
+      $("#development").show('1500');
     });
-
-    toggleWhatWeDo();
-});
-
-// function
-
-//  function to toggle what we do
-function toggleWhatWeDo() {
-    $(".service").on("click", function() {
-        if ($(".service-details", this).is(":visible")) {
-            $(".service-details", this).hide();
-            $(".service-card", this).fadeIn("slow");
-        } else {
-            $(".service-details", this).fadeIn("slow");
-            $(".service-card", this).hide();
-        }
+    $("#development").click(function(){
+      $("#development").slideUp('1500');
+      $("#development-image").slideDown('1500');
     });
-}
-
-// validate contact form
-
-function validateContactForm() {
-    let isFormValid = true;
-    const formInputs = contactForm.find("input,textarea");
-
-    formInputs.each(function() {
-        if (!$(this).val()) {
-            $(this).addClass("is-invalid");
-            isFormValid = false;
-        }
-    });
-    return isFormValid;
-}
-
-function removeFormErrors() {
-    contactForm.find("input,textarea").on("keydown", function() {
-        if ($(this).hasClass("is-invalid")) {
-            $(this).removeClass("is-invalid");
-        }
-    });
-}
-
-function clearForm() {
-    contactForm.find("input,textarea").each(function() {
-        $(this).val("");
-    });
-}
-
-removeFormErrors();
+  });
